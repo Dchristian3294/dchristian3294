fe9da933b2b1561a181244f4b449f715813d7b44
Dan Christian <dchrist6@lesley.edu>
2016-12-20 09:06:36 -0500

    asd

---
+++ dchristian3924.github.io/masterjava.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Daniel on 12/13/2016.
+ */
+console.log("The user has submitted their username information")
+
+function validate_form_data() {
+
+    var user_name = document.getElementById('user_name');
+    console.log('user_name');
+
+    var password = document.getElementById('password');
+    console.log('password');
+
+    var user_name_value = user_name.value ;
+    var password_value = password.value ;
+
+
+
+
+
+
+
+}
